7e3fe4593e81723d269cd64889cdf9b62926be7e
Iantsuma <ian.tsuyoshi@gmail,com>
2024-05-02 18:27:11 -0300

    final push

--- models/User.js
+++ models/User.js
@@ -16,10 +16,6 @@ const UserSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    },
-    CEP: {
-        type: String,
-        required: true
     }
 });
 
